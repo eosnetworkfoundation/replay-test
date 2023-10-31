97b835338048229f2685dda77ae81c50e6f0606d
Eric Passmore <eric.passmore@gmail.com>
2023-10-31 11:38:42 -0700

    reorganized scripts

---
+++ replay-client/install-nodeos.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+LEAP_VERSION="${1}"
+OS="ubuntu22.04"
+v4_DEB_FILE="leap_""${LEAP_VERSION}"-"${OS}""_amd64.deb"
+v5_DEB_FILE="leap_""${LEAP_VERSION}""_amd64.deb"
+if [ ${LEAP_VERSION:0:1} == "4" ]; then
+  DEB_URL="https://github.com/AntelopeIO/leap/releases/download/v""${LEAP_VERSION}"/"${v4_DEB_FILE}"
+else
+  DEB_URL="https://github.com/AntelopeIO/leap/releases/download/v""${LEAP_VERSION}"/"${v5_DEB_FILE}"
+fi
+
+TUID=$(id -ur)
+# must be root to run
+if [ "$TUID" -ne 0 ]; then
+  echo "Must run as root"
+  exit
+fi
+
+## root setup ##
+# clean out un-needed files
+for not_needed_deb_file in /tmp/leap_[0-9]*.deb
+do
+  if [ "${not_needed_deb_file}" != /tmp/"${DEB_FILE}" ]; then
+    echo "removing ${not_needed_deb_file}"
+    rm -rf ${not_needed_deb_file}
+  fi
+done
+
+# download file if needed
+if [ ! -f /tmp/"${DEB_FILE}" ]; then
+  wget --directory-prefix=/tmp "${DEB_URL}"
+fi
+# install nodeos
+dpkg -i /tmp/"${DEB_FILE}"
--- scripts/orchestrator-bootstrap.sh
+++ scripts/orchestrator-bootstrap.sh
@@ -2,19 +2,13 @@
 
 # setup aws cli
 sudo apt update
-apt install unzip python3
-sudo apt install unzip
+sudo apt install -y unzip python3 python3-pip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
 sudo ./aws/install
 
-BRANCH="ehp/gh-3-aws-setup"
-
 # get the scripts
-if [ -n "$BRANCH" ]; then
-  git clone -b "$BRANCH" https://github.com/eosnetworkfoundation/replay-test
-else
-  git clone https://github.com/eosnetworkfoundation/replay-test
-fi
+git clone https://github.com/eosnetworkfoundation/replay-test
+pip install datetime enum argparse werkzeug
 
 cd replay-test/orchestration-service || exit
 python3 ./web_service.py --config "../meta-data/test-simple-jobs.json" --host 0.0.0.0 &
