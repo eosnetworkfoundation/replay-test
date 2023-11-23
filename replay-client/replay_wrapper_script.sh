bf8a2d416396d54f48127e47c8608a05f0abe02f
Eric Passmore <eric.passmore@gmail.com>
2023-11-22 16:19:58 -0800

    orchestration uses static private ip

--- replay-client/replay_wrapper_script.sh
+++ replay-client/replay_wrapper_script.sh
@@ -3,14 +3,5 @@
 # simple wrapper script used by cronjob to run replay
 USER="enf-replay"
 
-IP_FILE=/home/"${USER}"/orchestration-ip.txt
-if [ ! -f "$IP_FILE" ]; then
-  echo "${IP_FILE} does not exist cannot source IP address and cannot connect to orchestration service"
-  exit 1
-fi
-ORCH_IP=$(cat ${IP_FILE})
-if [ -n "${ORCH_IP}" ]; then
-  echo "no ip address in file, ${IP_FILE} cannot connect to orchestration service"
-  exit 1
-fi
+ORCH_IP=172.31.79.129
 /home/${USER}/replay-test/replay-client/start-nodeos-run-replay.sh ${ORCH_IP} > /home/${USER}/last-replay.log 2>&1
--- scripts/run-replay-instance.sh
+++ scripts/run-replay-instance.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# launch replay nodes
+# launch replay nodes from command line
+# currently restricted by IAM permissions and NOT usable
 
 NUM_INSTANCES=${1:-1}
 DRY_RUN=${2}
@@ -11,11 +12,6 @@ else
   DRY_RUN=""
 fi
 
-# modify user data script to stuff in private ip of orchestration server
-ORCH_IP=$(sh /home/ubuntu/replay-test/scripts/get_private_ip.sh)
-sed "s^# MACRO_P echo \$ORCH_IP^echo $ORCH_IP^" /home/ubuntu/replay-test/scripts/replay-node-bootstrap.sh > /tmp/replay-node-bootstrap.sh
-mv /tmp/replay-node-bootstrap.sh /home/ubuntu/replay-test/scripts/replay-node-bootstrap.sh
-
-aws ec2 run-instances --launch-template LaunchTemplateName=ChickenReplayHost,Version=9 \
---user-data file:///home/ubuntu/replay-test/scripts/replay-node-bootstrap.sh \
---count $NUM_INSTANCES $DRY_RUN
+#aws ec2 run-instances --launch-template LaunchTemplateName=ChickenReplayHost,Version=9 \
+#--user-data file:///home/ubuntu/replay-test/scripts/replay-node-bootstrap.sh \
+#--count $NUM_INSTANCES $DRY_RUN
