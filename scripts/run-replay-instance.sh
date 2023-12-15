c254624e64452cd57a2bc80fd1dae6d1dc97be35
Eric Passmore <ericpassmore@users.noreply.github.com>
2023-12-14 16:13:24 -0800

    Add AWS instance terminate (#61)
    * aws instance terminate script
    * rm instance tracking file on termination
    * bug fix cmd as var not working
    * bug fix dry-run option messed up aws cli command
    * terminate script moved file check to ALL statement

--- scripts/run-replay-instance.sh
+++ scripts/run-replay-instance.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 # launch replay nodes from command line
-# currently restricted by IAM permissions and NOT usable
+
+HOME="/home/ubuntu"
+INSTANCE_FILE="${HOME}/aws-replay-instances.txt"
+SCRIPTS_DIR="${HOME}/replay-test/scripts"
+AWS_REPLAY_TEMPLATE="ChickenReplayHost"
+AWS_REPLAY_TEMPLATE_VERSION="13"
 
 NUM_INSTANCES=${1:-1}
 DRY_RUN=${2}
@@ -14,9 +19,13 @@ fi
 
 # modify user data script to stuff in private ip of orchestration server
 ORCH_IP=$(sh /home/ubuntu/replay-test/scripts/get_private_ip.sh)
-sed "s^# MACRO_P echo \$ORCH_IP^echo $ORCH_IP^" /home/ubuntu/replay-test/scripts/replay-node-bootstrap.sh > /tmp/replay-node-bootstrap.sh
-mv /tmp/replay-node-bootstrap.sh /home/ubuntu/replay-test/scripts/replay-node-bootstrap.sh
+sed "s^# MACRO_P echo \$ORCH_IP^echo $ORCH_IP^" "${SCRIPTS_DIR}/replay-node-bootstrap.sh" > /tmp/replay-node-bootstrap.sh
+mv /tmp/replay-node-bootstrap.sh "${SCRIPTS_DIR}/replay-node-bootstrap.sh"
 
-aws ec2 run-instances --launch-template LaunchTemplateName=ChickenReplayHost,Version=13 \
---user-data file:///home/ubuntu/replay-test/scripts/replay-node-bootstrap.sh \
---count $NUM_INSTANCES $DRY_RUN
+aws ec2 run-instances --launch-template LaunchTemplateName=${AWS_REPLAY_TEMPLATE},Version=${AWS_REPLAY_TEMPLATE_VERSION} \
+--user-data file://"${SCRIPTS_DIR}"/replay-node-bootstrap.sh \
+--count $NUM_INSTANCES $DRY_RUN > /tmp/aws-run-instance-out.json
+
+# append so you can run several times
+# terminate will clear out the instance file
+grep "InstanceId" /tmp/aws-run-instance-out.json | cut -d':' -f2 | tr -d ' ",' >> "$INSTANCE_FILE"
---
+++ scripts/terminate-replay-instance.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# kill off instances by ID.
+# When ALL provided terminates all instances in the file ~/aws-replay-instances.txt
+
+INSTANCE_FILE="/home/ubuntu/aws-replay-instances.txt"
+
+INSTANCE_ID=${1:ALL}
+DRY_RUN=${2}
+
+if [ -n "$DRY_RUN" ]; then
+  DRY_RUN="--dry-run"
+else
+  DRY_RUN="--no-dry-run"
+fi
+# remove hypens for later string compare
+DRY_RUN_CMPT=$(echo $DRY_RUN | tr -d  '-')
+
+# All instances
+if [ "$INSTANCE_ID" == "ALL" ]; then
+  # check file exists and has contents
+  if [[ -e "$INSTANCE_FILE" && -s "$INSTANCE_FILE" ]]; then
+    ALL_INSTANCES=$(paste -s -d ' ' "$INSTANCE_FILE")
+    aws ec2 terminate-instances "$DRY_RUN" --instance-ids "$ALL_INSTANCES"
+    # not dry-run remove file
+    if [ "$DRY_RUN_CMPT" == "nodryrun" ]; then
+      rm "$INSTANCE_FILE"
+    fi
+  fi
+# Single instance
+else
+  if [ -n "$INSTANCE_ID" ]; then
+     aws ec2 terminate-instances "$DRY_RUN" --instance-ids "$INSTANCE_ID"
+  else
+    echo "Must provide AWS instance ID or ALL as first arg"
+    exit 1
+  fi
+fi
