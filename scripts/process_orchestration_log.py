ab2a5aa1e918c41e42b4133092fd783a41f9a2c0
Eric Passmore <ericpassmore@users.noreply.github.com>
2023-12-15 13:59:22 -0800

    added CSV for times in block range (#65)

--- scripts/process_orchestration_log.py
+++ scripts/process_orchestration_log.py
@@ -1,16 +1,24 @@
 """Parse Orchestration File and Calculate Job Elapsed Time"""
 import argparse
+import sys
 from datetime import datetime
 import statistics
 import numpy as np
+from replay_configuration import ReplayConfigManager
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='helper script to extract elapsed timing from log')
     parser.add_argument('--log', type=str, help='path to config file')
+    parser.add_argument('--block-times', action=argparse.BooleanOptionalAction, \
+        default=False, help='collect times by block range')
+    parser.add_argument('--config', type=str, help='path to config file used in run')
 
     args = parser.parse_args()
     timings = []
+
+    if args.block_times and not args.config:
+        sys.exit("Must provide config with --config option")
 
     # Open the file and read log_entry by log_entry
     with open(args.log, 'r', encoding='utf-8') as file:
@@ -40,45 +48,55 @@ if __name__ == '__main__':
                 complete_record['total_minutes'] = int(timedelta.total_seconds())/60
                 timings.append(complete_record)
 
-    # Calculate average (mean)
-    average = statistics.mean(list(record['total_minutes'] for record in timings))
-
-    # Calculate standard deviation
-    std_dev = statistics.stdev(list(record['total_minutes'] for record in timings))
-
-    # Calculate median
-    median = statistics.median(list(record['total_minutes'] for record in timings))
-
-    # Calculate the 75th and 90th percentiles
-    percentile_75 = np.percentile(list(record['total_minutes'] for record in timings), 75)
-    percentile_90 = np.percentile(list(record['total_minutes'] for record in timings), 90)
-
-    # get longest
-    longest = max(list(record['total_minutes'] for record in timings))
-
-    # Print the results
-    print("JOB TIMING ALL TIMES IN MINUTES")
-    print("-------------------------------")
-    print(f"Number of Jobs: {len(timings)}")
-    print(f"Average: {round(average,2)}")
-    print(f"Standard Deviation: {round(std_dev,2)}")
-    print(f"Median: {round(median,2)}")
-    print(f"75th Percentile: {round(percentile_75,2)}")
-    print(f"90th Percentile: {round(percentile_90,2)}")
-    print(f"Longest Running Job {round(longest,2)} mins")
-
-    if std_dev > average:
-        print("\nLONG RUNNING JOBS TOP 90%")
-        print("-------------------------")
+    if args.block_times:
+        replay_config_manager = ReplayConfigManager(args.config)
         for record in timings:
-            if record['total_minutes'] > percentile_90:
-                # when config and snapshot exist print full
-                # field check for backwards compat
-                if 'config' in record and 'snapshot' in record \
-                    and record['config'] and record['snapshot']:
-                    print(f"Job {record['jobid']}\
+            block_manager = replay_config_manager.get(record['config'])
+            config = block_manager.as_dict()
+            block_span = config['end_block_id'] - config['start_block_id']
+            average_time = round(record['total_minutes'] / block_span,2)
+            blocks_for_3_hours = round(60 * 3 / average_time,0)
+            print (f"{config['start_block_id']}, {config['end_block_id']}, {average_time}, {blocks_for_3_hours}")
+    else:
+        # Calculate average (mean)
+        average = statistics.mean(list(record['total_minutes'] for record in timings))
+
+        # Calculate standard deviation
+        std_dev = statistics.stdev(list(record['total_minutes'] for record in timings))
+
+        # Calculate median
+        median = statistics.median(list(record['total_minutes'] for record in timings))
+
+        # Calculate the 75th and 90th percentiles
+        percentile_75 = np.percentile(list(record['total_minutes'] for record in timings), 75)
+        percentile_90 = np.percentile(list(record['total_minutes'] for record in timings), 90)
+
+        # get longest
+        longest = max(list(record['total_minutes'] for record in timings))
+
+        # Print the results
+        print("JOB TIMING ALL TIMES IN MINUTES")
+        print("-------------------------------")
+        print(f"Number of Jobs: {len(timings)}")
+        print(f"Average: {round(average,2)}")
+        print(f"Standard Deviation: {round(std_dev,2)}")
+        print(f"Median: {round(median,2)}")
+        print(f"75th Percentile: {round(percentile_75,2)}")
+        print(f"90th Percentile: {round(percentile_90,2)}")
+        print(f"Longest Running Job {round(longest,2)} mins")
+
+        if std_dev > average:
+            print("\nLONG RUNNING JOBS TOP 90%")
+            print("-------------------------")
+            for record in timings:
+                if record['total_minutes'] > percentile_90:
+                    # when config and snapshot exist print full
+                    # field check for backwards compat
+                    if 'config' in record and 'snapshot' in record \
+                        and record['config'] and record['snapshot']:
+                        print(f"Job {record['jobid']}\
  running time {round(record['total_minutes'],2)}\
  config {record['config']} with snapshot {record['snapshot']}")
-                else:
-                    print(f"Job {record['jobid']}\
+                    else:
+                        print(f"Job {record['jobid']}\
  running time {round(record['total_minutes'],2)}")
